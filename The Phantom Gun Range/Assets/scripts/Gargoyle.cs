fb4ad92ad61be1b79b61604b39fd1233c4c12306
Dryden001 <51544458+Dryden001@users.noreply.github.com>
2022-04-12 23:57:19 -0600

    added gargoyle script

---
+++ The Phantom Gun Range/Assets/scripts/Gargoyle.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Gargoyle : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    public GameObject player;
+    private string gargoylestate = "search";
+    public Animator gargoyleanim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = this.GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void FixedUpdate(){
+        if(gargoylestate == "idle"){
+            idle();
+        }else if(gargoylestate == "search"){
+            search();
+        }else if(gargoylestate == "reached"){
+            reached();
+        }
+    }
+    private void idle(){
+        if(Vector3.Distance(this.transform.position, player.transform.position) >= 1){
+            gargoylestate = "search";
+            gargoyleanim.SetInteger("Gargoyleanimstate", 1);
+        }
+    }
+    private void search(){
+        agent.destination = player.transform.position;
+        if(Vector3.Distance(this.transform.position, player.transform.position) < 1){
+            gargoylestate = "reached";
+        }
+    }
+    private void reached(){
+        gargoyleanim.SetInteger("Gargoyleanimstate", 0);
+    }
+}
